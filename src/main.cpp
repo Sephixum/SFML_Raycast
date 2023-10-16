b77e0073c548609ff15b7664fdf242bf88ae0c76
Sephixum <emotive.5353@gmail.com>
2023-10-16 07:59:42 +0330

    Small changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 #include <Game.hpp>
 #include <iostream>
 
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 int main() {
   Game game;
+  std::cout << "raycasting project version : "
+            << Raycasting_engine_VERSION_MAJOR << "."
+            << Raycasting_engine_VERSION_MINOR << "."
+            << Raycasting_engine_VERSION_PATCH << '\n';
   while (game.running()) {
     game.update();
     game.render();
